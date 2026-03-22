cca063008cff690722dddc83b577246c993c873b
itbitgit <jricha2006@gmail.com>
2026-03-22 12:35:07 +0530

    fix api key for streamlit cloud

--- color_palette_generator.py
+++ color_palette_generator.py
@@ -3,8 +3,10 @@ import json
 from groq import Groq
 from dotenv import load_dotenv
 
+
 load_dotenv()
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+import streamlit as st
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 CATEGORY_HINTS = {
     "Interior Design":    "Use livable, balanced tones like warm neutrals, earthy accents and calming shades.",
